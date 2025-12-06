7239da16565f5aeb7e14acbb308df7df93cf90a1
BlackSerge <sergiosaborit99@gmail.com>
2025-12-06 05:11:34 -0500

    configurar los statics

--- tienda-veterinaria-backend/backend/settings.py
+++ tienda-veterinaria-backend/backend/settings.py
@@ -9,8 +9,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # SECURITY / DEBUG
 # ----------------------------------------
